b595278debb49b23578117d7c486f457380421dc
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-04-16 23:00:02 +0200

    corretta posizione script jquery nell'html  /  aggiunto document.ready al JS

--- js/script.js
+++ js/script.js
@@ -5,179 +5,180 @@ le icone disponibili come da layout
 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 */
 
-const icons = [
-    {
-        name: 'cat',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'crow',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'dog',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'dove',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'dragon',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'horse',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'hippo',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'fish',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas'
-    },
-    {
-        name: 'carrot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas'
-    },
-    {
-        name: 'apple-alt',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas'
-    },
-    {
-        name: 'lemon',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas'
-    },
-    {
-        name: 'pepper-hot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas'
-    },
-    {
-        name: 'user-astronaut',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas'
-    },
-    {
-        name: 'user-graduate',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas'
-    },
-    {
-        name: 'user-ninja',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas'
-    },
-    {
-        name: 'user-secret',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas'
+
+$(document).ready(function () {
+    const icons = [
+        {
+            name: 'cat',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'crow',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dog',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dove',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dragon',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'horse',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'hippo',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'fish',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'carrot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'apple-alt',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'lemon',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'pepper-hot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'user-astronaut',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-graduate',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-ninja',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-secret',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        }
+    ];
+    
+    const colors = [
+        'blue',
+        'orange',
+        'purple'
+    ];
+    
+    main();
+    
+    //creo una copia dell'array originale che contiene anche i colori per non modificare quello di partenza
+    function colorify (arrayColor, array){
+        let iconsCopy = [];
+        for (const obj of array) {
+            switch (obj.type) {
+                case "animal":
+                    iconsCopy.push({ ...obj, color: arrayColor[0] });
+                    break;
+                case "vegetable":
+                    iconsCopy.push({ ...obj, color: arrayColor[1] });
+                    break;
+                case "user":
+                    iconsCopy.push({ ...obj, color: arrayColor[2] });
+                    break;
+            }
+        }
+        return iconsCopy;
     }
-];
-
-const colors = [
-    'blue',
-    'orange',
-    'purple'
-];
-
-main();
-
-//creo una copia dell'array originale che contiene anche i colori per non modificare quello di partenza
-function colorify (arrayColor, array){
-    let iconsCopy = [];
-    for (const obj of array) {
-        switch (obj.type) {
-            case "animal":
-                iconsCopy.push({ ...obj, color: arrayColor[0] });
-                break;
-            case "vegetable":
-                iconsCopy.push({ ...obj, color: arrayColor[1] });
-                break;
-            case "user":
-                iconsCopy.push({ ...obj, color: arrayColor[2] });
-                break;
+    //itero sul nuovo array
+    function populateHtml (array){
+        //prendo il div che devo popolare
+        const cardsContainer = $(".cards-container");
+        cardsContainer.html('');
+        //itero sull'array degli oggetti (che contiene anche i colori)
+        for (const icon of array) {
+            //estrapolo le informazioni che mi servono per creare le mie cards
+            let {name, prefix, family, color} = icon;
+            //definisco tramite i backticks e i valori degli oggetti che aspetto avranno le cards nella pagina html
+            const iconaHtml = `
+                <div class="card">
+                    <i class='${family} ${prefix}${name} ${color}'></i>
+                    <h5>${name}</h5>
+                </div>`;
+    
+            //aggiungo a cardsContainer
+            cardsContainer.append(iconaHtml);
         }
     }
-    return iconsCopy;
-}
-
-
-//itero sul nuovo array
-function populateHtml (array){
-    //prendo il div che devo popolare
-    const cardsContainer = $(".cards-container");
-    cardsContainer.html('');
-    //itero sull'array degli oggetti (che contiene anche i colori)
-    for (const icon of array) {
-        //estrapolo le informazioni che mi servono per creare le mie cards
-        let {name, prefix, family, color} = icon;
-        //definisco tramite i backticks e i valori degli oggetti che aspetto avranno le cards nella pagina html
-        const iconaHtml = `
-            <div class="card">
-                <i class='${family} ${prefix}${name} ${color}'></i>
-                <h5>${name}</h5>
-            </div>`;
-
-        //aggiungo a cardsContainer
-        cardsContainer.append(iconaHtml);
+    //funzione che filtra l'array di colori in base alla scelta dell'utente
+    function filtrify(array, currentChoice) {
+        if (currentChoice == "all") {
+            console.log(array);
+            return array;
+        }
+        const filteredArray = array.filter((obj) => {
+            return obj.type == currentChoice;
+        });
+        console.log(filteredArray);
+        return filteredArray;
     }
-}
-//funzione che filtra l'array di colori in base alla scelta dell'utente
-function filtrify(array, currentChoice) {
-    if (currentChoice == "all") {
-        console.log(array);
-        return array;
+    //main
+    function main() {
+        const coolorArray = colorify(colors, icons);
+        populateHtml(coolorArray);
+        //le option del select
+        const valoreSelect = $("#valore");
+        //quando cambia
+        valoreSelect.change(function () {
+            const currentChoice = this.value;
+            //filtra gli elementi nell array di partenza in base al select
+            const pickedArray = filtrify(coolorArray, currentChoice);
+            //funzione che popola la pagina HTML
+            populateHtml(pickedArray);
+        });
     }
-    const filteredArray = array.filter((obj) => {
-        return obj.type == currentChoice;
-    });
-    console.log(filteredArray);
-    return filteredArray;
-}
-//main
-function main() {
-    const coolorArray = colorify(colors, icons);
-    populateHtml(coolorArray);
-    //le option del select
-    const valoreSelect = $("#valore");
-    //quando cambia
-    valoreSelect.change(function () {
-        const currentChoice = this.value;
-        //filtra gli elementi nell array di partenza in base al select
-        const pickedArray = filtrify(coolorArray, currentChoice);
-        //funzione che popola la pagina HTML
-        populateHtml(pickedArray);
-    });
-}
+})
 
 
